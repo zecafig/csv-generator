80dd29f1d34c25cafa9add26b02384bbf81eb764
Sueki <vitor.sueki@gmail.com>
2018-02-28 16:15:55 -0300

    Improvements

--- csv-generator.py
+++ csv-generator.py
@@ -7,18 +7,20 @@ lines = int(input("How many lines? "))
 
 fk = Faker("pt_BR")
 
-z = open("%s.csv" % datetime.datetime.now().strftime("%Y%m%d%H%M%S"), "w")
+filename = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+z = open("%s.csv" % filename, "w")
 w = csv.writer(z)
-w.writerow(("id", "name", "address", "occupation", "company", "cpf", "email", "birthday"))
+w.writerow(("email", "name", "mother", "mobile", "gender", "birthday"))
 
 for i in range(lines):
     w.writerow((
-    	i+1, 
-    	fk.name(), 
-    	fk.street_address(), 
-    	random.choice(["Pilot", "Teacher", "Fighter", "Driver"]), 
-    	random.choice(["Apple", "IBM", "Google", "Netflix"]), 
-    	fk.ssn(), 
     	fk.email(), 
+    	fk.name(), 
+    	fk.name(), 
+        fk.phone_number(),
+    	"male" if random.randrange(2) == 0 else "female", 
     	fk.date()
     ))
+
+print("Exported file: %s.csv" % filename )
